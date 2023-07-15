7741e2ef0e23f92fea1ccea1875fd5e1f9e219e7
Dachi <88776708+Dachi13@users.noreply.github.com>
2023-07-15 15:14:24 +0400

    Add files via upload

---
+++ PinguLabirinth/pingu interfaces/src/fop/w8inter/Animal.java
@@ -0,0 +1,9 @@
+package fop.w8inter;
+
+public abstract class Animal {
+    protected int age;
+
+    public void birthday() {
+        age++;
+    }
+}
---
+++ PinguLabirinth/pingu interfaces/src/fop/w8inter/Penguin.java
@@ -0,0 +1,24 @@
+package fop.w8inter;
+
+public class Penguin extends Animal implements Comparable, Copyable {
+    private String name;
+    private double height;
+
+    public Penguin(int age, String name, double height) {
+        this.age = age;
+        this.name = name;
+        this.height = height;
+    }
+
+    public Penguin copy() {
+        return new Penguin(age, name, height);
+    }
+
+    public int compareTo(Penguin other) {
+        if (age != other.age)
+            return age - other.age;
+        if (height != other.height)
+            return (int) Math.signum(height - other.height);
+        return name.compareTo(other.name);
+    }
+}
